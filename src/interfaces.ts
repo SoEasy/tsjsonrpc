b678bef3da2b7ed4ec492bdbb45eb84ab677552a
vhaldemario <vhaldemario@gmail.com>
2019-03-01 17:21:58 +0500

    1.4.2 - Убрал toString метод у ошибки, чтобы библиотека не была виновата в косяках разработчиков

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,75 +1,73 @@
 export type IJsonRpcMethodConfigPayload<TMethodConfigPayload> = {
-  [key in keyof TMethodConfigPayload]: TMethodConfigPayload[key];
+  [key in keyof TMethodConfigPayload]: TMethodConfigPayload[key]
 }
 
 export interface IJsonRpcMethodConfigBase {
-  method: string;
+  method: string
 }
 
-export type IJsonRpcMethodConfig<TMethodConfigPayload> = IJsonRpcMethodConfigPayload<TMethodConfigPayload> & IJsonRpcMethodConfigBase;
+export type IJsonRpcMethodConfig<
+  TMethodConfigPayload
+> = IJsonRpcMethodConfigPayload<TMethodConfigPayload> & IJsonRpcMethodConfigBase
 
 export interface IJsonRpcServiceConfig {
-  apiServerUrl: string;
-  httpClient?: IHttpClient;
+  apiServerUrl: string
+  httpClient?: IHttpClient
 }
 
 export interface IJsonRpcRequest {
-  id: string;
-  jsonrpc: string;
-  method: string;
-  params?: object;
+  id: string
+  jsonrpc: string
+  method: string
+  params?: object
 }
 
 export interface IJsonRpcResponse {
-  id: string;
-  jsonrpc: string;
+  id: string
+  jsonrpc: string
   // TODO мб дженерики? Но боюсь в них утонуть
   // tslint:disable-next-line
-  result?: any;
+  result?: any
   // tslint:disable-next-line
-  error?: any;
+  error?: any
 }
 
 export interface IHttpClient {
   // tslint:disable-next-line
-  post(url: string, params: IJsonRpcRequest): any;
+  post(url: string, params: IJsonRpcRequest): any
 }
 
 export interface IJsonRpcErrorBody {
-  code: number;
-  message: string;
-  data?: any;
+  code: number
+  message: string
+  data?: any
 }
 
 export class TsJsonRpcError extends Error {
-  status!: number;
-  statusText!: string;
-  rpcError!: IJsonRpcErrorBody;
+  status!: number
+  statusText!: string
+  rpcError!: IJsonRpcErrorBody
 
   static makeHttpError(status: number, statusText: string): TsJsonRpcError {
-    const retVal = new TsJsonRpcError();
-    retVal.status = status;
-    retVal.statusText = statusText;
+    const retVal = new TsJsonRpcError()
+    retVal.status = status
+    retVal.statusText = statusText
 
-    return retVal;
+    return retVal
   }
 
   static makeRpcError(rpcError: any): TsJsonRpcError {
-    const retVal = new TsJsonRpcError();
-    retVal.rpcError = rpcError;
+    const retVal = new TsJsonRpcError()
+    retVal.rpcError = rpcError
 
-    return retVal;
+    return retVal
   }
 
-  toString = (): string => {
-    return JSON.stringify(this);
-  };
-
   get isNetworkError(): boolean {
-    return !!this.status;
+    return !!this.status
   }
 
   get isRpcError(): boolean {
-    return !!this.rpcError;
+    return !!this.rpcError
   }
 }
